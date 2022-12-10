9b6338fdcfae4554caf49ae91a44b38792560f67
protondk <edgarjosue2911@gmail.com>
2022-12-10 17:49:28 -0600

    Images showing. Webpack devServer config was missing. I added it so that the images would load. I did not use the aliases because I could not make them work. I will try later to fix that. Finally, added webpack rule to load svg images.

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,62 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/'
-	},
-	mode: 'development',
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader'
-					}
-				]
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: [
-					"style-loader",
-					"css-loader",
-					"sass-loader",
-				],
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			filename: './index.html'
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		}),
-	],
-	devServer: {
-		historyApiFallback: true,
-	}
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|jpeg|webp)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/pictures/[hash][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  devServer: {
+    //use static instead of contentBase
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3005,
+    historyApiFallback: true,
+  },
+};
