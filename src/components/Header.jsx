bd9f867d1dc7812f8b7752030a2feb28db9b0da1
protondk <edgarjosue2911@gmail.com>
2022-12-19 14:22:50 -0600

    Cart now renders default products inside. Still have to adjust to show the products added by user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useContext } from "react";
 import "../styles/Header.scss";
 import Menu from "../components/Menu";
+import MyOrder from "../containers/MyOrder";
 import AppContext from "../context/AppContext";
 
 import menu from "../assets/icons/icon_menu.svg";
@@ -10,6 +11,7 @@ import shoppingCart from "../assets/icons/icon_shopping_cart.svg";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
   const { state } = useContext(AppContext);
 
   const handleToggle = () => {
@@ -49,13 +51,17 @@ const Header = () => {
           <li className="navbar-email" onClick={handleToggle}>
             platzi@example.com
           </li>
-          <li className="navbar-shopping-cart">
+          <li
+            className="navbar-shopping-cart"
+            onClick={() => setToggleOrders(!toggleOrders)}
+          >
             <img src={shoppingCart} alt="shopping cart" />
             {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
       {toggle && <Menu></Menu>}
+      {toggleOrders && <MyOrder></MyOrder>}
     </nav>
   );
 };
