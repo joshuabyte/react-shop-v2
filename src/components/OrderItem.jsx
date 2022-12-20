85b405a7dcce13b1a561cd89ee0a82c450e0bba6
protondk <edgarjosue2911@gmail.com>
2022-12-19 18:37:33 -0600

    Cart now renders selected products and also calculates de total price of the selected products.

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,19 +1,19 @@
 // @ts-nocheck
-import React from 'react';
-import '../styles/OrderItem.scss';
-import closeIcon from "../assets/icons/icon_close.png"
+import React from "react";
+import "../styles/OrderItem.scss";
+import closeIcon from "../assets/icons/icon_close.png";
 
-const OrderItem = () => {
-	return (
-		<div className="OrderItem">
-			<figure>
-				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
-			</figure>
-			<p>Bike</p>
-			<p>$30,00</p>
-			<img src={closeIcon} alt="close" />
-		</div>
-	);
-}
+const OrderItem = ({ product }) => {
+  return (
+    <div className="OrderItem">
+      <figure>
+        <img src={product.images[0]} alt={product.title} />
+      </figure>
+      <p>{product.title}</p>
+      <p>${product.price}</p>
+      <img src={closeIcon} alt="close" />
+    </div>
+  );
+};
 
 export default OrderItem;
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -7,6 +7,14 @@ import arrow from "../assets/icons/flechita.svg";
 
 const MyOrder = () => {
   const { state } = useContext(AppContext);
+
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
+
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -14,14 +22,14 @@ const MyOrder = () => {
         <p className="title">My order</p>
       </div>
       <div className="my-order-content">
-		{state.cart.map(product => (
-			<OrderItem product={product} key={`orderItem-${product.id}`} />
-		))}
+        {state.cart.map((product) => (
+          <OrderItem product={product} key={`orderItem-${product.id}`} />
+        ))}
         <div className="order">
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
