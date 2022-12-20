645fdc1dafc6b90f661445f436565dc900e531f1
protondk <edgarjosue2911@gmail.com>
2022-12-19 20:52:44 -0600

    Cart now removes products from checkout. Platzi's React Course finished

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,9 +1,16 @@
 // @ts-nocheck
-import React from "react";
+import React, { useContext } from "react";
+import AppContext from "../context/AppContext";
 import "../styles/OrderItem.scss";
 import closeIcon from "../assets/icons/icon_close.png";
 
 const OrderItem = ({ product }) => {
+  const { removeFromCart } = useContext(AppContext);
+
+  const handleRemove = (product) => {
+    removeFromCart(product);
+  };
+
   return (
     <div className="OrderItem">
       <figure>
@@ -11,7 +18,10 @@ const OrderItem = ({ product }) => {
       </figure>
       <p>{product.title}</p>
       <p>${product.price}</p>
-      <img src={closeIcon} alt="close" />
+      <span className="closeIcon">
+      <img src={closeIcon} alt="close" onClick={() => handleRemove(product)} />
+      </span>
+
     </div>
   );
 };
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -8,16 +8,25 @@ const initialState = {
 const useInitialState = () => {
   const [state, setState] = useState(initialState);
 
+  //In order to remove items with the same id, the addToCart and removeFromCart functions were modified. addToCart now creates a unique id for each product added.
   const addToCart = (payload) => {
     setState({
       ...state,
-      cart: [...state.cart, payload],
+      cart: [...state.cart, { ...payload, idCart: state.cart.length + 1 }],
+    });
+  };
+
+  const removeFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((items) => items.idCart !== payload.idCart),
     });
   };
 
   return {
     state,
     addToCart,
+    removeFromCart,
   };
 };
 
