41c68bae8aca3ea150fc1e2fd7555cb00d96caa0
protondk <edgarjosue2911@gmail.com>
2022-12-20 00:06:15 -0600

    Deploy attempt #3

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
+    filename: "react-shop-v2/bundle.js",
+    publicPath: "/react-shop-v2/",
   },
   mode: "development",
   resolve: {
